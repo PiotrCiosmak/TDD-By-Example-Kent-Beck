4adc33d2ce3fa91d98b776f9e347ed251b47b89f
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-04-08 21:31:29 +0200

    chapter eleven done

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -12,8 +12,18 @@ class MoneyTest
     }
 
     @Test
-    public void testDifferentClassEquality()
+    public void testEquality()
     {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    @Test
+    public void testMultiplication()
+    {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 }
