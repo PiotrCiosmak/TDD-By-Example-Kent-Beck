7b44064df514fcc30e54aec666bddc4ec4e585d1
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-04-08 18:21:51 +0200

    chapter two done

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -5,9 +5,9 @@ public class Dollar
         this.amount = amount;
     }
 
-    void times(int multiplier)
+    Dollar times(int multiplier)
     {
-        amount *= multiplier;
+        return new Dollar(amount * multiplier);
     }
 
     int amount;
