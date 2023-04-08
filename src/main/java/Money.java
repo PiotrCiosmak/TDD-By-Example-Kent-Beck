fe7f2332286170943d8d1527ecb09444a0bdd2b9
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-04-08 19:51:47 +0200

    chapter eight done

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,5 +1,17 @@
-public class Money
+abstract public class Money
 {
+    public static Money dollar(int amount)
+    {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount)
+    {
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);
+
     @Override
     public boolean equals(Object object)
     {
